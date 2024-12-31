6f7032a1027cccdd83fa0444fd169cc0dab1395f
Simone3838 <simone.paolini14@gmail.com>
2024-12-31 21:55:48 +0100

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,6 @@
 // kernel.cpp
+extern "C" void shell_main();
+
 extern "C" void kernel_main() {
-    const char *str = "Hello, World!";
-    char *video_memory = (char*) 0xb8000;
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        video_memory[i * 2] = str[i];
-        video_memory[i * 2 + 1] = 0x07; // Light gray on black
-    }
-
-    while (1);
+    shell_main();
 }
